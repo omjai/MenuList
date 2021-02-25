538d6da45d54e7e83b43a5f7f9efbb15a8c91c91
omjai <jaiom.143@gmail.com>
2021-02-25 10:23:58 +0530

    Initial Commit

---
+++ MenuList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MenuList
+//
+//  Created by APPLE on 25/02/21.
+//  Copyright © 2021 APPLE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
